52c7b1e64da12eeba09b681e0f24628201e0046a
Matheus Vieira <matheus.vieira@wehandle.com.br>
2025-08-09 14:32:00 -0300

    Add TypeScript configuration file (tsconfig.json) for project setup

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  collectCoverageFrom: ['src/**/*.ts'],
+  coverageThreshold: {
+    global: {
+      branches: 70,
+      functions: 70,
+      lines: 70,
+      statements: 70
+    }
+  }
+};
